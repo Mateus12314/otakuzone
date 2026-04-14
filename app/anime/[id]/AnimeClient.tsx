97cc9d760f30801721320324f8448031b4359a57
Mateus <seuemail@gmail.com>
2026-04-14 18:15:07 -0300

    add anime client

---
+++ app/anime/[id]/AnimeClient.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function AnimeClient({ id }: { id: string }) {
+  const [anime, setAnime] = useState<any>(null);
+
+  useEffect(() => {
+    async function load() {
+      const res = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
+      const data = await res.json();
+      setAnime(data.data);
+    }
+
+    load();
+  }, [id]);
+
+  if (!anime) {
+    return <p style={{ color: "#fff", padding: 20 }}>Carregando...</p>;
+  }
+
+  return (
+    <div style={{ background: "#000", color: "#fff", minHeight: "100vh", padding: 20 }}>
+      <h1 style={{ color: "red" }}>{anime.title}</h1>
+
+      <img
+        src={anime.images.jpg.large_image_url}
+        style={{ width: 300, borderRadius: 10 }}
+      />
+
+      <p style={{ marginTop: 20 }}>{anime.synopsis}</p>
+
+      <p>⭐ Nota: {anime.score}</p>
+      <p>🎬 Episódios: {anime.episodes}</p>
+    </div>
+  );
+}|
--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -1,37 +1,5 @@
-"use client";
-import { useEffect, useState } from "react";
+import AnimeClient from "./AnimeClient";
 
-export default function AnimePage({ params }: { params: { id: string } }) {
-  const [anime, setAnime] = useState<any>(null);
-
-  useEffect(() => {
-    async function load() {
-      const res = await fetch(`https://api.jikan.moe/v4/anime/${params.id}`);
-      const data = await res.json();
-      setAnime(data.data);
-    }
-
-    load();
-  }, [params.id]);
-
-  if (!anime) {
-    return <p style={{ color: "#fff", padding: 20 }}>Carregando...</p>;
-  }
-
-  return (
-    <div style={{ background: "#000", color: "#fff", minHeight: "100vh", padding: 20 }}>
-      <h1 style={{ color: "red" }}>{anime.title}</h1>
-
-      <img
-        src={anime.images.jpg.large_image_url}
-        style={{ width: 300, borderRadius: 10 }}
-      />
-
-      <p style={{ marginTop: 20 }}>{anime.synopsis}</p>
-
-      <p>⭐ Nota: {anime.score}</p>
-      <p>🎬 Episódios: {anime.episodes}</p>
-    </div>
-  );
+export default function Page({ params }: any) {
+  return <AnimeClient id={params.id} />;
 }
-
